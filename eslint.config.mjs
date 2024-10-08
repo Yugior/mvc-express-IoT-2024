658d762c6b4dc8f1a6e9324dd91e4ac2a2e0610d
Yugior <horasvihe@gmail.com>
2024-10-08 17:05:00 -0600

    A

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
 
 
 export default [
@@ -10,4 +12,5 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended
 ];
