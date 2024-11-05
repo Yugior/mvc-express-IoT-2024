4866061abf94c5ebed56e06c04a08bb0bfb52eff
Yugior <horasvihe@gmail.com>
2024-11-05 17:00:57 -0600

    Instalación

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ["dist/"] },
-  { files: ["src/**/*.{js,ts,jsx,tsx}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.browser } },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
 ];
