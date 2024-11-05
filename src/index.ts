cb48d3b32d942003d60d4cba08e1d6482fd25176
Yugior <horasvihe@gmail.com>
2024-11-05 17:26:20 -0600

    X

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import dotenvFlow from "dotenv-flow";
 import express from "express";
-import unknownError from "./middlewares/unknown-resources";
 import studentRouter from "./routes/student";
-
+import testRoutes from "./routes/test";
+import unknownResource from "./middlewares/unknown-resources";
+import unknownError from "./middlewares/unknown-error";
+import validationError from "./middlewares/validation-error";
 //Para poder acceder a las variables del ambiente (.env)
+//if(process.env.NODE_ENV!=="production"){
 if (process.env.NODE_ENV !== "production") {
   dotenvFlow.config();
 }
@@ -12,11 +15,19 @@ const app = express();
 
 // Middleware para parsear JSON
 app.use(express.json());
-
+// Rutas de api
 app.use("/api/v1/student", studentRouter);
 
+//Rutas de prueva
+app.use("/error", testRoutes);
+
+// Middlewares
+app.use(validationError);
+app.use(unknownResource); //Error 404
+
+//Middleware de error
 app.use(unknownError);
 
-app.listen(3001, function () {
-  console.log("Escuchando puerto " + 3001);
+app.listen(process.env.SERVER_PORT, function () {
+  console.log("Escuchando puerto " + process.env.SERVER_PORT);
 });
--- src/routes/student.ts
+++ src/routes/student.ts
@@ -1,20 +1,26 @@
 // routes/usuarios.ts
 import { Router } from "express";
-import validate from "../middlewares/validate";
-import { studentSchema } from "../schemas/student";
 import {
   createStudent,
   deleteStudent,
   getStudent,
   updateStudent,
 } from "../controllers/student";
+import validate from "../middlewares/validate";
+import { studentSchema } from "../schemas/student";
 
 const router = Router();
 
 // Regresa todos los alumnos en la base de datos
-
 router.get("/", getStudent);
+
+// Crea un alumno nuevo en la BD
 router.post("/", validate(studentSchema), createStudent);
-router.delete("/:id", deleteStudent);
+
+// Actualiza los datos de un alumno en la BD
 router.put("/:id", validate(studentSchema), updateStudent);
+
+// Borra los datos de un alumno en la BD
+router.delete("/:id", deleteStudent);
+
 export default router;
